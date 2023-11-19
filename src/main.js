c3be17489ac4934c8bc951d7052a31ed871da0ac
Sophia <leesophia011@gmail.com>
2023-11-19 15:11:25 +0900

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 $(window).on('scroll',function(){
     if(window.scrollY > 78){
-      $('.navbar-brand').css("font-size", "32px");
       $('.header').addClass('header--dark')
     }else if(window.scrollY < 78){
-      $('.navbar-brand').css("font-size", "25px");
       $('.header').removeClass('header--dark')
     }
   })
@@ -22,6 +20,7 @@ document.addEventListener('scroll', () => {
 // Arrow up버튼을 아래로 스크롤시 투명하게 처리함
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
+  //home screen에 절반정도가 스크롤 되면
   if (window.scrollY > homeHeight / 2) {
     arrowUp.style.opacity = 1;
   } else {
